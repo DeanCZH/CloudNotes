ccc2f33747968d180b93016261aa430e9c322b6a
alvin-mei <alvin-m@foxmail.com>
2015-10-27 22:43:54 +0800

    初始框架

---
+++ src/main/database/database.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE IF NOT EXISTS db_usermanage CHARACTER  SET UTF8;
+USE db_usermanage;
+
+#用户信息表
+CREATE TABLE IF NOT EXISTS tb_user(
+user_id INT(10) NOT NULL AUTO_INCREMENT,
+user_name VARCHAR(20) NOT NULL UNIQUE ,
+user_password VARCHAR(20) NOT NULL,
+user_email VARCHAR(50) NOT NULL,
+user_phone VARCHAR (20),
+user_createtime DATETIME ,
+user_lastlogintime TIMESTAMP,
+PRIMARY key (user_id)
+)ENGINE=InnoDB DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
+#NOTE
+CREATE TABLE IF NOT EXISTS tb_note(
+note_id int(10) NOT NULL AUTO_INCREMENT,
+note_userid int(10) NOT NULL,
+note_bookid int(10) NOT NULL,
+note_title varchar(200) NOT NULL,
+note_content TEXT NOT NULL,
+note_createtime DATETIME NOT NULL,
+note_updatetime DATETIME NOT NULL,
+PRIMARY key (note_id)
+)ENGINE=InnoDB DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
+#BOOK
+CREATE TABLE IF NOT EXISTS tb_book(
+book_id int(10) NOT NULL AUTO_INCREMENT,
+book_userid int(10) NOT NULL,
+book_title varchar(200) NOT NULL,
+book_desc varchar(2000) NOT NULL,
+book_notenum int(10) NOT NULL DEFAULT '0' COMMENT '笔记总数',
+note_createtime DATETIME NOT NULL,
+PRIMARY key (book_id)
+)ENGINE=InnoDB DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
---
+++ src/main/java/com/mjl/model/User.java
@@ -0,0 +1,12 @@
+package com.mjl.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Alvin on 15/10/27.
+ */
+public class User implements Serializable{
+    private int userId;
+    private String userName;
+    private String passWord;
+}
